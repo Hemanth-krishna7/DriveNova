06fc65e70ed6906be53fd2bdf7666961f55f5bdb
Hemanth Krishna <dandamudihk@gmail.com>
2026-07-03 18:21:06 +0530

    Changing vite.config.js file for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    sourcemap: false,
+
+    chunkSizeWarningLimit: 1200,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+          ],
+
+          three: [
+            "three",
+            "@react-three/fiber",
+            "@react-three/drei",
+          ],
+
+          motion: [
+            "framer-motion",
+          ],
+        },
+      },
+    },
+  },
+});
