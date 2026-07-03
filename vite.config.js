3885aed3c8af240d393f56319b7b6bb4a0ce6b8b
Hemanth Krishna <dandamudihk@gmail.com>
2026-07-03 18:33:37 +0530

    Reverting the vite.config.js file for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
-  build: {
-    sourcemap: false,
-
-    chunkSizeWarningLimit: 1200,
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: [
-            "react",
-            "react-dom",
-            "react-router-dom",
-          ],
-
-          three: [
-            "three",
-            "@react-three/fiber",
-            "@react-three/drei",
-          ],
-
-          motion: [
-            "framer-motion",
-          ],
-        },
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
